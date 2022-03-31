3c2c16675931555733f49576b7958becd4895b5d
hjw4595 <hongjung73@gmail.com>
2022-03-31 16:42:24 +0900

    main page 겉 틀 구성 완

--- components/page/Nav/index.tsx
+++ components/page/Nav/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from "next/link";
 
 import styles from './nav.module.css'
 
@@ -9,27 +10,37 @@ import Wallet from '/svg/wallet.svg'
 function Nav() {
 return (
     <div className={styles.wrapepr}>
-        <div>Logo</div>
-        <div className={styles['nav-index']}>
-            <div className={styles.item}>
-                ARTWork
-            </div>
-            <div className={styles.item}>
-                Collections
-            </div>
-            <div className={styles.item}>
-                Edtions
-            </div>
-            <div className={styles.item}>
-                Resources
-            </div>      
-            <div className={styles['svg-item']}>
-            <Image src={Account} alt='account' width={24} height={24}  />
-            </div>      
-            <div className={styles['svg-item']}>
-            <Image src={Wallet} alt='wallet' width={24} height={24}  />
-            </div>
+        <div className={styles['logo']}>
+            <div>logo</div>
         </div>
+        <ul className={styles['nav-index']}>
+            <li className={styles.item}>
+                <Link href="/artwork">
+                    ARTWork
+                </Link>
+            </li>
+            <li className={styles.item}>
+                <Link href="/collections">
+                    Collections
+                </Link>
+            </li>
+            <li className={styles.item}>
+                <Link href="/edtions">
+                    Edtions
+                </Link>
+            </li>
+            <li className={styles.item}>
+                <Link href="/resources">
+                    Resources
+                </Link>
+            </li>      
+            <li className={styles['svg-item']}>
+            <Image src={Account} alt='account' width={24} height={24}  />
+            </li>      
+            <li className={styles['svg-item']}>
+            <Image src={Wallet} alt='wallet' width={24} height={24}  />
+            </li>
+        </ul>
     </div>
 )
 }
